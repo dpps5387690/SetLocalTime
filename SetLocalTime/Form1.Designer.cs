fcc5f710b15813d114727ab393e9ab7963a774a0
dpps5387690 <dpps5387690@gmail.com>
2022-07-20 14:16:50 +0800

    First

---
+++ SetLocalTime/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace SetLocalTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btGetTime = new System.Windows.Forms.Button();
+            this.btSetTime = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_Week = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(295, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // btGetTime
+            // 
+            this.btGetTime.Location = new System.Drawing.Point(313, 11);
+            this.btGetTime.Name = "btGetTime";
+            this.btGetTime.Size = new System.Drawing.Size(75, 25);
+            this.btGetTime.TabIndex = 1;
+            this.btGetTime.Text = "GetTime";
+            this.btGetTime.UseVisualStyleBackColor = true;
+            this.btGetTime.Click += new System.EventHandler(this.btGetTime_Click);
+            // 
+            // btSetTime
+            // 
+            this.btSetTime.Location = new System.Drawing.Point(313, 42);
+            this.btSetTime.Name = "btSetTime";
+            this.btSetTime.Size = new System.Drawing.Size(75, 25);
+            this.btSetTime.TabIndex = 1;
+            this.btSetTime.Text = "SetTime";
+            this.btSetTime.UseVisualStyleBackColor = true;
+            this.btSetTime.Click += new System.EventHandler(this.btSetTime_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lb_Week, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 38);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(112, 29);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Week";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lb_Week
+            // 
+            this.lb_Week.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Week.AutoSize = true;
+            this.lb_Week.Location = new System.Drawing.Point(59, 8);
+            this.lb_Week.Name = "lb_Week";
+            this.lb_Week.Size = new System.Drawing.Size(49, 13);
+            this.lb_Week.TabIndex = 0;
+            this.lb_Week.Text = "...";
+            this.lb_Week.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            this.notifyIcon1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 77);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btSetTime);
+            this.Controls.Add(this.btGetTime);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "SetLocalTime";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btGetTime;
+        private System.Windows.Forms.Button btSetTime;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_Week;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ SetLocalTime/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Globalization;
+using Microsoft.Win32;
+
+namespace SetLocalTime
+{
+
+    public partial class Form1 : Form
+    {
+        [DllImport("Kernel32.dll")]
+        public static extern bool SetLocalTime(ref SystemTime sysTime);
+
+        public struct SystemTime
+        {
+            public ushort wYear;
+            public ushort wMonth;
+            public ushort wDayOfWeek;
+            public ushort wDay;
+            public ushort wHour;
+            public ushort wMinute;
+            public ushort wSecond;
+            public ushort wMiliseconds;
+        }
+        public static bool SetLocalTimeByStr(string timestr)
+        {
+            bool successful = false;
+            SystemTime sysTime = new SystemTime();
+            try
+            {
+                DateTime dt = Convert.ToDateTime(timestr);   // 將字串轉成DateTime格式
+
+                sysTime.wYear = Convert.ToUInt16(dt.Year);
+                sysTime.wMonth = Convert.ToUInt16(dt.Month);
+                sysTime.wDay = Convert.ToUInt16(dt.Day);
+                sysTime.wHour = Convert.ToUInt16(dt.Hour);
+                sysTime.wMinute = Convert.ToUInt16(dt.Minute);
+                sysTime.wSecond = Convert.ToUInt16(dt.Second);
+                successful = SetLocalTime(ref sysTime);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("SetLocalTimeByStr Error" + e.Message);
+            }
+            return successful;
+        }
+        public Form1()
+        {
+            
+            InitializeComponent();
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            //dateTimePicker1.CustomFormat = "'Today is:' hh:mm:ss dddd MMMM dd, yyyy";
+            dateTimePicker1.CustomFormat = "yyyy/MM/dd HH:mm:ss";
+            Get_WeekOfYear();
+        }
+        private void Get_WeekOfYear()
+        {
+            // Gets the Calendar instance associated with a CultureInfo.
+            CultureInfo myCI = new CultureInfo("en-US");
+            Calendar myCal = myCI.Calendar;
+            // Gets the DTFI properties required by GetWeekOfYear.
+            CalendarWeekRule myCWR = myCI.DateTimeFormat.CalendarWeekRule;
+            DayOfWeek myFirstDOW = myCI.DateTimeFormat.FirstDayOfWeek;
+
+            lb_Week.Text = String.Format("{0}", myCal.GetWeekOfYear(DateTime.Now, myCWR, myFirstDOW));
+        }
+
+        private void btGetTime_Click(object sender, EventArgs e)
+        {
+            dateTimePicker1.Value = DateTime.Now;
+            Get_WeekOfYear();
+        }
+
+        private void btSetTime_Click(object sender, EventArgs e)
+        {
+            SetLocalTimeByStr(dateTimePicker1.Value.ToString());
+            Get_WeekOfYear();
+        }
+
+        //開機自動啟動
+        public static void RegisterInStartup(string name, string path, bool onStartOpen)
+        {
+            try
+            {
+                var registryKey = Registry.CurrentUser.OpenSubKey
+                    ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                if (onStartOpen)
+                    registryKey.SetValue(name, path);//註冊打開
+                else
+                    registryKey.DeleteValue(name);//刪除註冊
+            }
+            catch (Exception e)
+            {
+                //https://www.ruyut.com/
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseMove(object sender, MouseEventArgs e)
+        {
+            notifyIcon1.Text = "Week:" + lb_Week.Text;
+        }
+    }
+}
